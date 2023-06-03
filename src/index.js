55a50b7c696a27f20ff85d1e86ebb84a99e8553b
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-03 14:31:37 +0100

    Webpack configuration

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './style.css';
+import Icon from './assets/icon.png';
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  devServer: {
+    static: './dist',
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
